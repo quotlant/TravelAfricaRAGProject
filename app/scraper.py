4defe18ef1152badc2eb14cc7f9b610d45f20249
quotlant <peteramoro@icloud.com>
2026-07-07 17:48:05 +0300

    get more hotel details

--- app/scraper.py
+++ app/scraper.py
@@ -2,34 +2,60 @@ import requests
 from bs4 import BeautifulSoup #this is a library that allows us to parse HTML and XML documents.
 import pandas as pd
 
-url = "https://www.safaribookings.com/tours/kenya/lodge"
-
-response = requests.get(url, timeout=10)
-
-print(response.status_code)
-#print(response.text[:500]) this will print the first 500 characters of the HTML response
-
-soup = BeautifulSoup(response.text, 'html.parser') 
-
-titles = soup.find_all("div", class_="holder-tourtitle")
-
-print(f"i found {len(titles)} tours")
-
-data = []
 
 
-for title in titles:
-    safari = {
-        "title": title.get_text(strip=True),
-    } #this will create a dictionary with the title of the safari tour
+
+safaris = []
+
+for page in range(1,4):
+    if page == 1:
+        url = "https://www.safaribookings.com/tours/kenya/lodge"
+    else:
+        url = f"https://www.safaribookings.com/tours/kenya/lodge?page={page}"
     
-    data.append(safari) #this will append the dictionary to the data list
+    print(f"Scraping page {page} of safari tours...")
     
-df = pd.DataFrame(data) #this will create a dataframe from the data list
+    response = requests.get(url, timeout=10)
+        
 
-df.to_csv("safari_tours.csv", 
+    soup = BeautifulSoup(response.text, 'html.parser') 
+
+    titles = soup.find_all("div", 
+                        class_="holder-tourtitle")
+
+    details = soup.find_all( "div",
+                            class_= "list__item--data" 
+    )
+
+    operators = soup.find_all("div",
+                            class_="operator-snippet__data"
+                            )
+
+
+
+    for i in range(len(titles)):
+        safari = {
+            "title": titles[i].get_text(" ", strip=True),
+        #this will create a dictionary with the title of the safari tour
+        "details": (details[i].get_text(" ", strip=True)
+        if i < len(details)
+        else " "
+        ),
+        
+        "operator": (operators[i].get_text(" ", strip=True)
+        if i < len(operators)
+        else " "
+        ),
+        
+        }
+        safaris.append(safari) #this will append the dictionary to the data list
+        
+df = pd.DataFrame(safaris) #this will create a dataframe from the data list
+df.to_csv("data/safari_tours.csv", 
           index=False) #this will save the dataframe to a csv file without the index column
 
-print(f"saved {len(data)} safari tours to safari_tours.csv")
+print(f"saved {len(safaris)} safari tours to safari_tours.csv")
+
+
     
     
